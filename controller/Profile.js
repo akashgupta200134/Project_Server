41e2b52d9c3d257529c00bf1090fd2b45e866d60
Akash Gupta <akashgupta_ce_2021@ltce.in>
2025-08-18 18:22:28 +0530

    account deletion

--- controller/Profile.js
+++ controller/Profile.js
@@ -4,7 +4,13 @@ const profile = require("../models/profile");
 exports.createProfile = async (req, res) => {
   try {
     // data fetch
-    const { gender, dateofBirth = "", profession, about = "", contactNumber} = req.body;
+    const {
+      gender,
+      dateofBirth = "",
+      profession,
+      about = "",
+      contactNumber,
+    } = req.body;
 
     //fetch  id
     const id = req.user.id;
@@ -33,11 +39,52 @@ exports.createProfile = async (req, res) => {
 
     // save the data into db
     await profileDetails.save();
+
+    // return success response
+    return res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      profileDetails,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
       message: error.message,
+    });
+  }
+};
+
+// delete account
+exports.deleteAccount = async (req, res) => {
+  try {
+    //fetch  id
+    const id = req.user.id;
+    const userDetails = await User.findById(id);
+
+    // validation
+    if (!userDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // delete account
+    await profile.findByIdAndDelete({ _id: userDetails.additionalDetials });
+
+    await User.findByIdAndDelete({ _id: id });
+
+    // return success response
+    return res.status(200).json({
+      success: true,
+      message: "Account deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while deleting user",
     });
   }
 };
